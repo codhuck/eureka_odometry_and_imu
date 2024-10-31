141b0f07c462f7d22cc16cafbdd31a7d2e088a0d
codhuck <rslvklmc@gmail.com>
2024-11-01 01:59:59 +0300

    f

--- launch/ekf.launch.py
+++ launch/ekf.launch.py
@@ -1,4 +1,5 @@
 from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
 import launch_ros.actions
 import os
 import yaml
@@ -6,48 +7,14 @@ from launch.substitutions import EnvironmentVariable
 import pathlib
 import launch.actions
 from launch.actions import DeclareLaunchArgument
-from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    robot_localization_dir = get_package_share_directory('robot_localization')
-    parameters_file_dir = os.path.join(robot_localization_dir, 'params')
-    parameters_file_path = os.path.join(parameters_file_dir, 'dual_ekf_navsat.yaml')
-    os.environ['FILE_PATH'] = str(parameters_file_dir)
     return LaunchDescription([
-        launch.actions.DeclareLaunchArgument(
-            'output_final_position',
-            default_value='false'),
-        launch.actions.DeclareLaunchArgument(
-            'output_location',
-	    default_value='~/dual_ekf_navsat_example_debug.txt'),
-	
-    launch_ros.actions.Node(
-            package='robot_localization', 
-            executable='ekf_node', 
-            name='ekf_filter_node_odom',
-	        output='screen',
-            parameters=[parameters_file_path],
-            remappings=[('odometry/filtered', 'odometry')]           
+        launch_ros.actions.Node(
+            package='robot_localization',
+            executable='ekf_node',
+            name='ekf_filter_node',
+            output='screen',
+            parameters=[os.path.join(get_package_share_directory("robot_localization"), 'params', 'ekf.yaml')],
            ),
-    launch_ros.actions.Node(
-            package='robot_localization', 
-            executable='ekf_node', 
-            name='ekf_filter_node_map',
-	        output='screen',
-            parameters=[parameters_file_path],
-            remappings=[('odometry/filtered', 'odometry')]
-           ),           
-    launch_ros.actions.Node(
-            package='robot_localization', 
-            executable='navsat_transform_node', 
-            name='navsat_transform',
-	        output='screen',
-            parameters=[parameters_file_path],
-            remappings=[('imu/data', 'camera1/camera1/imu'),
-                        ('gps/fix', 'gps/fix'), 
-                        ('gps/filtered', 'gps/filtered'),
-                        ('odometry/gps', 'odometry/gps'),
-                        ('odometry/filtered', 'odometry/global')]           
-
-           )           
 ])
